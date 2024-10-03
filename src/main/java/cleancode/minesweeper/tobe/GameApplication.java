03bcf01a9ba8516ea9dae885eda0851c444650c2
Hoyun Jung <jhy9094@gmail.com>
2024-10-03 19:07:56 +0900

    섹션4 - LSP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,11 +1,11 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
 
 public class GameApplication {
     public static void main(String[] args) {
-        GameLevel gameLevel = new Advanced();
+        GameLevel gameLevel = new Middle();
         Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
