1cb5d6b3a48abb9fc08ba54a8651bc07a89f8d32
Hoyun Jung <jhy9094@gmail.com>
2024-10-03 19:16:52 +0900

    섹션4 - ISP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,12 +1,13 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.gamelevel.Middle;
 
 public class GameApplication {
     public static void main(String[] args) {
-        GameLevel gameLevel = new Middle();
+        GameLevel gameLevel = new Beginner();
         Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.initialize();
         minesweeper.run();
     }
 }
