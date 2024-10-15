d693343ed529f55ca482cc07c4224fca2e6199ac
Hoyun Jung <jhy9094@gmail.com>
2024-10-15 23:07:15 +0900

    섹션7 - 리팩토링 (2) - 객체의 책임과 응집도
    - PassOrder 개념 추출

--- src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
@@ -50,7 +50,7 @@ public class StudyCafeSeatPass implements StudyCafePass {
         return price;
     }
 
-    public double getDiscountRate() {
-        return discountRate;
+    public int getDiscountPrice() {
+        return (int) (price * discountRate);
     }
 }
